2a4a9c9c27a2cac18a714f2652da2b11c837d126
Gabriel Sevilha <58699454+gabrielsevilha1@users.noreply.github.com>
2020-10-15 21:09:17 -0300

    Add files via upload

--- exemples/2d_terrain_exemple.c
+++ exemples/2d_terrain_exemple.c
@@ -4,13 +4,11 @@
 
 int main(){
 
-	float x = 200, y = 400;
-	
+	float object_x = 400, object_y = 300;
 	float terrain[] = {0,300, 100,400, 200,400, 200,500, 400,500, 450,450, 500,425, 550,375, 600,350, 650,375, 700,425, 750,500, 800,500};
-
-	float height = getHeight2D(x,y,terrain,sizeof(terrain));
+	
+	float height = getHeight2D(object_x,object_y,terrain,sizeof(terrain));
 	
 	printf("%f\n",height);
-
 	return 0;
 }
